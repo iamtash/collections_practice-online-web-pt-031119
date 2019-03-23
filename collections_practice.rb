769ab32e578f60881b04d94dc9138262c2610c0d
Natasha Chernis <natashachernis@gmail.com>
2019-03-23 04:00:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,5 +9,13 @@ def sort_array_desc(integers_array)
 end
 
 def sort_array_char_count(string_array)
-  
+  string_array.sort do |a,b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
 end
